9045d3ee541f52ad4a5b53ea8d5762a9e9cc37d2
Heather Shapiro <hshapiro93@gmail.com>
2017-03-29 15:02:32 -0400

    removed bot.on

--- app.js
+++ app.js
@@ -34,18 +34,18 @@ const pmkbClient = new PMKBClient(configs.get('PMKB_HOST'), configs.get('PMKB_US
 //=========================================================
 // Bots Dialogs
 //=========================================================
-bot.on('conversationUpdate', function (message) {
-    if (message.membersAdded) {
-        message.membersAdded.forEach(function (identity) {
-            if (identity.id === message.address.bot.id) {
-                var reply = new builder.Message()
-                    .address(message.address)
-                    .text('Hi! I am SpeechToText Bot. I can understand the content of any audio and convert it to text. Try sending me a wav file.');
-                bot.send(reply);
-            }
-        });
-    }
-});
+// bot.on('conversationUpdate', function (message) {
+//     if (message.membersAdded) {
+//         message.membersAdded.forEach(function (identity) {
+//             if (identity.id === message.address.bot.id) {
+//                 var reply = new builder.Message()
+//                     .address(message.address)
+//                     .text('Hi! I am SpeechToText Bot. I can understand the content of any audio and convert it to text. Try sending me a wav file.');
+//                 bot.send(reply);
+//             }
+//         });
+//     }
+// });
 
 bot.dialog('/', function (session) {
   session.send(prompts.greetMsg);
